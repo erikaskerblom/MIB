ce591428fd4464cf0fcf4d96de99135e8d66a68b
filiperlingmark <filip.erlingmark@hotmail.com>
2021-12-20 15:03:12 +0100

    lol

--- src/StartRaket/Validering.java
+++ src/StartRaket/Validering.java
@@ -4,10 +4,23 @@
  */
 package StartRaket;
 
+import javax.swing.JFormattedTextField;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author filip
  */
 public class Validering {
-    
+
+    public static boolean textFaltHarVarde(JFormattedTextField rutaAttKolla) {
+        boolean resultat = true;
+
+        if (rutaAttKolla.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Inmatningsrutan är tom!");
+            resultat = false;
+        }
+
+        return resultat;
+    }
 }
