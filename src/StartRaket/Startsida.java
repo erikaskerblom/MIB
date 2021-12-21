a097c3e8103e07639da9f27de46f138aff743aa6
erikaskerblom <erik.askerblom@hotmail.com>
2021-12-21 13:36:00 +0100

    Massa saker
    Jag har gjort massvis av bra saker

--- src/StartRaket/Startsida.java
+++ src/StartRaket/Startsida.java
@@ -25,35 +25,27 @@ public class Startsida extends javax.swing.JFrame {
     private void initComponents() {
 
         lblValkommen = new javax.swing.JLabel();
-        cbAnvandare = new javax.swing.JComboBox<>();
-        btnBekrafta = new javax.swing.JButton();
+        btnAgent = new javax.swing.JButton();
+        btnAlien = new javax.swing.JButton();
+        btnAdmin = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        lblValkommen.setText("Välkommen!");
+        lblValkommen.setFont(new java.awt.Font("Arial Nova", 1, 18)); // NOI18N
+        lblValkommen.setText("MIB Sektor Skandinavien");
 
-        cbAnvandare.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Agent", "Alien", "Admin" }));
-        cbAnvandare.addAncestorListener(new javax.swing.event.AncestorListener() {
-            public void ancestorMoved(javax.swing.event.AncestorEvent evt) {
-            }
-            public void ancestorAdded(javax.swing.event.AncestorEvent evt) {
-                cbAnvandareAncestorAdded(evt);
-            }
-            public void ancestorRemoved(javax.swing.event.AncestorEvent evt) {
-            }
-        });
-        cbAnvandare.addActionListener(new java.awt.event.ActionListener() {
+        btnAgent.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnAgent.setText("Agent");
+        btnAgent.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cbAnvandareActionPerformed(evt);
+                btnAgentActionPerformed(evt);
             }
         });
 
-        btnBekrafta.setText("OK");
-        btnBekrafta.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnBekraftaActionPerformed(evt);
-            }
-        });
+        btnAlien.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        btnAlien.setText("Alien");
+
+        btnAdmin.setText("Administratör");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -62,48 +54,46 @@ public class Startsida extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(82, 82, 82)
-                        .addComponent(lblValkommen))
+                        .addGap(123, 123, 123)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btnAlien, javax.swing.GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE)
+                            .addComponent(btnAgent, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(25, 25, 25)
+                                .addComponent(btnAdmin))))
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(31, 31, 31)
-                        .addComponent(cbAnvandare, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addGap(36, 36, 36)
-                        .addComponent(btnBekrafta)))
-                .addContainerGap(193, Short.MAX_VALUE))
+                        .addGap(106, 106, 106)
+                        .addComponent(lblValkommen, javax.swing.GroupLayout.PREFERRED_SIZE, 228, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(106, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(35, 35, 35)
-                .addComponent(lblValkommen)
-                .addGap(43, 43, 43)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(cbAnvandare, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(btnBekrafta))
-                .addContainerGap(183, Short.MAX_VALUE))
+                .addGap(19, 19, 19)
+                .addComponent(lblValkommen, javax.swing.GroupLayout.PREFERRED_SIZE, 41, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 26, Short.MAX_VALUE)
+                .addComponent(btnAgent, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnAlien, javax.swing.GroupLayout.PREFERRED_SIZE, 53, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addComponent(btnAdmin)
+                .addGap(36, 36, 36))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void cbAnvandareActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbAnvandareActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cbAnvandareActionPerformed
-
-    private void cbAnvandareAncestorAdded(javax.swing.event.AncestorEvent evt) {//GEN-FIRST:event_cbAnvandareAncestorAdded
-        // TODO add your handling code here:
-    }//GEN-LAST:event_cbAnvandareAncestorAdded
-
-    private void btnBekraftaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBekraftaActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnBekraftaActionPerformed
+    private void btnAgentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAgentActionPerformed
+        new LoggaIN().setVisible(true);
+    }//GEN-LAST:event_btnAgentActionPerformed
 
  
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnBekrafta;
-    private javax.swing.JComboBox<String> cbAnvandare;
+    private javax.swing.JButton btnAdmin;
+    private javax.swing.JButton btnAgent;
+    private javax.swing.JButton btnAlien;
     private javax.swing.JLabel lblValkommen;
     // End of variables declaration//GEN-END:variables
 }
